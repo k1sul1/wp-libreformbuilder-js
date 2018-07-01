eb792b56f2103b576dd16fb28dbeab37eb1e70bd
Christian Nikkanen <christian.nikkanen@vincit.com>
2018-07-01 13:19:51 +0300

    Add event listeners

--- app/scenes/builder/builder-scene.js
+++ app/scenes/builder/builder-scene.js
@@ -1,14 +1,52 @@
 import './styles.scss'
 
 import React, { Component } from 'react'
+import { connect } from 'kea'
+import builderLogic from '../../scenes/builder/logic'
 import Builder from '../../components/builder/builder'
 
+@connect({
+  actions: [
+    builderLogic, [
+      'export',
+      'import',
+    ]
+  ],
+})
 export default class BuilderScene extends Component {
+  state = {
+    contentEl: null,
+    stateInput: null,
+  }
+
+  componentDidMount () {
+    // Dirty DOM work
+    const publishButton = document.querySelector('#publish')
+
+    if (publishButton) {
+      console.log('Apparently in wp-admin')
+
+      const stateInput = document.querySelector('[name="wplfb-state"]')
+      const contentEl = document.querySelector('#content')
+
+      this.actions.import(stateInput.value)
+      this.setState({ contentEl, stateInput })
+
+      publishButton.addEventListener('click', (e) => { // Not intending to remove
+        console.log('I will not let you save')
+        this.actions.export({ stateInput, contentEl })
+        e.preventDefault()
+        e.stopPropagation()
+      }, { once: true, capture: true, passive: false })
+    }
+  }
+
   render () {
+    const { contentEl, stateInput } = this.state
+    const isWP = contentEl && stateInput
+
     return (
-      <main>
-        <Builder />
-      </main>
+      <Builder isWP={isWP} />
     )
   }
 }
