18ae2671eb7fd4a4ffa8465131739ae4ffb51e2f
Christian Nikkanen <christian.nikkanen@vincit.com>
2018-08-16 16:52:31 +0300

    Fix devbuild and add font awesome

--- config-overrides.js
+++ config-overrides.js
@@ -1,11 +1,11 @@
 const rewireMobX = require('react-app-rewire-mobx')
-const rewireCss = require('react-app-rewire-css-modules')
+const rewireSass = require('react-app-rewire-scss')
 // const { injectBabelPlugin } = require('react-app-rewired')
 
 module.exports = function override (config, env) {
   // config = injectBabelPlugin('transform-decorators', config)
   config = rewireMobX(config, env)
-  config = rewireCss(config, env)
+  config = rewireSass(config, env)
 
   return config
 }
